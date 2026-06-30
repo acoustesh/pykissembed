0eb4940e6a4001db176543559ed7923b97f62858
acoustesh <acoustesh@alliende.org>
2026-06-29 20:00:06 -0400

    bump version to 0.1.5

--- pykissembed_local/pykissembed_local/__init__.py
+++ pykissembed_local/pykissembed_local/__init__.py
@@ -4,6 +4,6 @@ from __future__ import annotations
 
 from pykissembed_local.provider import LocalProvider
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 __all__ = ["LocalProvider", "__version__"]
